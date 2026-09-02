039e7c1de50fbe016a458d6bff344870e9d20c38
juspreet62 <juspreet2023@gmail.com>
2026-09-02 12:42:09 +0530

    Bug 28 fixed

--- pset_1/javascript/bug28.js
+++ pset_1/javascript/bug28.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: err.message });
-});
+const app = express();
 
 app.get('/crash', (req, res) => {
   throw new Error('Boom');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
 });
 
 module.exports = app;
