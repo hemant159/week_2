a458eec07ec6c4d38a6200d7ebc96ad6ef75d86e
ayashkant2023-alt <ayashkant2023@gift.edu.in>
2026-09-02 12:53:54 +0530

    Fixed the bug in the line no 6

--- pset_1/javascript/bug24.js
+++ pset_1/javascript/bug24.js
@@ -3,12 +3,21 @@ const app = express();
 
 function logger(req, res, next) {
   console.log(`${req.method} ${req.url}`);
+  next();
 }
 
 app.use(logger);
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
+});
+
+app.listen(3000, () => {
+  console.log('Bug24 running on port 3000');
 });
 
 module.exports = app;
